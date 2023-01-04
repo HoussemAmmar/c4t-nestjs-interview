28b53427b1e84b1f704c8f82100f6d3a0ed6cf45
HoussemAmmar <houssem.ammar.06@gmail.com>
2023-01-04 12:04:04 +0100

    implement auth module schema

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { AbstractService } from '../abstract/abstract.service';
+import { Auth } from './auth.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 
 @Injectable()
-export class AuthService {}
+export class AuthService extends AbstractService<Auth> {
+
+  constructor(
+    @InjectModel(Auth.name) private authModel: Model<Auth>,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+
+  ) {
+    super(authModel, logger);
+  }
+  protected modelName = Auth.name;
+}
